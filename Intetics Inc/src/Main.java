13d44aa2ad78df0be1df4e8bbee0e4f97ac58a99
Medentsiy Yaroslav <medentsiy.y@gmail.com>
2018-02-14 18:15:36 +0200

    add ciklum interview test

--- Intetics Inc/src/Main.java
+++ Intetics Inc/src/Main.java
@@ -1,18 +1,49 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
         int firstMultiplier=0;
-        int secondMultiplier=0;
+        int secondMultiplier=0;/*
+        System.out.println(isPalindrome(999949999));
+        System.out.println(isPrimeNumber(69143));
+        System.out.println(isPrimeNumber(91009));*/
+        StringBuilder out=new StringBuilder(" ") ;
+        int c ;
+        try {
+            InputStreamReader reader = new InputStreamReader(new FileInputStream("C:\\Ph\\1test.txt"));
+            
+            while (true){
+                if ((c=reader.read())!=-1) {
+                    out.append((char) c);
+                    System.out.print((char) c);
+                }
+            }
+
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+//            e.printStackTrace();
+        }
+
+
+        System.exit(0);
         int sum = 0;
         Date start = new Date();
         for (int i = 0; i <=99999; i++) {
             if (isPrimeNumber(i)) {
-                for (int j = 99999; j>=0 ; j--) {
+                for (int j = 0; j<=99999 ; j++) {
                     if (isPrimeNumber(j)) {
                         int k =i*j;
                         if (isPalindrome(k)&&k>sum){
+                            System.out.println("Sum"+ sum+ " : New sum"+ k );
                             sum=k;
                             firstMultiplier=i;
                             secondMultiplier=j;
@@ -42,7 +73,7 @@ public class Main {
             return false;
         if (number==2) return true;
         if (number%2==0) return false;
-        for (int i = 3; i*i <=number; i+=2) {
+        for (int i = 3; i<=number; i++) {
             if (number % i == 0) {
                 return false;
             }
